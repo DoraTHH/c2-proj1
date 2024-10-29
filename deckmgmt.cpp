fa5120af33868c438bf7dd366246f175b4a9b024
DoraTHH <erikerikssonsepost@gmail.com>
2024-10-29 11:13:37 +0100

    this took way longer than it reasonably should have

--- deckmgmt.cpp
+++ deckmgmt.cpp
@@ -1,6 +1,32 @@
-
 #include <iostream>
 #include <string.h>
+#include <bits/stdc++.h>
+
+void generateIndecies(int input[], int size, int number);
+
+int main () {
+   int deckIndecies[52]; //this is the main array that will be used to point to cards in mainDeck
+   generateIndecies(deckIndecies, 52, 1); //initializes and shuffles the array
+
+   for (int i = 0; i < 52; i++) { //debug feature, will be deleted
+    std::cout << deckIndecies[i] << "\n";
+   }
+}
+
+void generateIndecies(int input[], int size, int number) { //function that puts values in an array then shuffles said array
+    number--; //refers to the number of decks. will be relevant when generating the negative deck
+    for (int i = 0; i < size; i++) {
+        input[i] = i + (size * number);
+    };
+    std::random_shuffle(input, input+size);
+}; //somehow it just worked the first time i compiled it
+
+
+
+
+
+
+
 
 class card
 {
@@ -28,11 +54,10 @@ class deck {
         }
 };
 
-int main()
+void deckCreationTest()
 {
     deck myDeck;
     for (int i = 0; i < 52; i++) {
         std::cout << "\nsuit: " << myDeck.cards[i].suit << "value: " << myDeck.cards[i].value;
     }
-    return 0;
-}
+};
